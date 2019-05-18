c72629703c99bab2cebcd9d1397bbb4a97d4aba5
joeshub <mojomail@gmail.com>
2019-05-17 23:14:57 -0700

    updates sequence arrow

--- src/hooks/useStyles.js
+++ src/hooks/useStyles.js
@@ -1,36 +1,27 @@
 import { useEffect } from 'react'
 
 const defaults = {
-    '__app_visibility': 'visible',
-
-    '__color_bg': '#505050',
-    '__color_fg': '#f5f5f5',
-    '__color_black': '#000',
-    '__color_dark_grey': '#2e2e2e',
-    '__color_light_grey': '#5d5d5d',
-    '__color_highlight': '#37e147',
-
-    '__base_font_size': 12,
-
-    '__number_of_steps': 16,
-    '__number_of_tracks': 4,
-
-    '__grid_unit': 2,
-
-    '__play_head_width': 2,
-
+    '__app_visibility'     : 'visible',
+    '__color_bg'           : '#505050',
+    '__color_fg'           : '#f5f5f5',
+    '__color_black'        : '#000',
+    '__color_dark_grey'    : '#2e2e2e',
+    '__color_light_grey'   : '#5d5d5d',
+    '__color_highlight'    : '#37e147',
+    '__base_font_size'     : 12,
+    '__number_of_steps'    : 16,
+    '__number_of_tracks'   : 4,
+    '__grid_unit'          : 2,
+    '__play_head_width'    : 2,
     '__form_element_height': 30,
-    '__input_bpm_width': 45,
-
-    '__step_height': 32,
-
-    '__track_title_width': 120,
-
-    '__note_width': 32,
-    '__note_height': 49,
-    '__note_border_size': 1,
-    '__note_margin_vert': 5,
-    '__note_margin_horz': 4,
+    '__input_bpm_width'    : 45,
+    '__step_height'        : 32,
+    '__track_title_width'  : 120,
+    '__note_width'         : 32,
+    '__note_height'        : 49,
+    '__note_border_size'   : 1,
+    '__note_margin_vert'   : 5,
+    '__note_margin_horz'   : 4,
 }
 
 const noteWidthFull = defaults.__note_width + defaults.__note_margin_horz * 2
@@ -38,9 +29,7 @@ const noteHeightFull = defaults.__note_height + defaults.__note_margin_vert * 2
 
 const getNotesAreaWidthInPixels = (totalSteps) => noteWidthFull * totalSteps - (defaults.__play_head_width / 2)
 
-const setProperty = (key, value) => {
-    document.documentElement.style.setProperty(key, value)
-}
+const setProperty = (key, value) => document.documentElement.style.setProperty(key, value)
 
 const setStyles = (totalSteps) => {
     defaults.__number_of_steps = totalSteps
@@ -53,8 +42,6 @@ const setStyles = (totalSteps) => {
     setProperty('--color-highlight', defaults.__color_highlight)
     setProperty('--base-font-size', defaults.__base_font_size + 'px')
     setProperty('--number-of-steps', defaults.__number_of_steps)
-    setProperty('--aspect-ratio', defaults.__aspect_ratio)
-    setProperty('--grid-unit', defaults.__grid_unit + 'px')
     setProperty('--play-head-width', defaults.__play_head_width + 'px')
     setProperty('--play-head-height', noteHeightFull * defaults.__number_of_tracks - (defaults.__note_margin_vert * 2) + 'px')
     setProperty('--spacer', defaults.__grid_unit * 5 + 'px')
