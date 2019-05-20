fa13599614c0a21bb28d7c2ff4083295a3b378a2
joeshub <mojomail@gmail.com>
2019-05-19 21:18:54 -0700

    adds aria-label to Toolbar

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -33,13 +33,13 @@ const ToolBar = ({
 
     return (
         <nav className="toolbar">
-            <button className="form_element button_stop" onClick={stopPlayback}>
+            <button className="form_element button_stop" onClick={stopPlayback} aria-label="Stop">
                 <svg width="14" height="14" viewBox="0 0 14 14">
                     <rect className="button_icon_path" x="2" y="2" width="10" height="10" />
                 </svg>
 
             </button>
-            <button className="form_element button_play_pause" onClick={togglePlayback}>
+            <button className="form_element button_play_pause" onClick={togglePlayback} aria-label="Play / Pause">
                 <svg width="14" height="14" viewBox="8 8 20 20">
                     {isSequencePlaying && <path className="button_icon_path" id="pause-icon" data-state="playing" d="M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26" />}
                     {!isSequencePlaying && <path className="button_icon_path" id="play-icon" data-state="paused" d="M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28" />}
@@ -51,6 +51,7 @@ const ToolBar = ({
                 className="form_element select_sequence"
                 value={selectedSequenceID}
                 onChange={e => selectSequence(+e.target.value)}
+                aria-label="Select sequence"
             >
                 {
                     sequenceList.map(seq => {
