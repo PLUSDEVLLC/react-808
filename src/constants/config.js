1b1f195dce895f28e7dba5c5403888947b080287
joeshub <mojomail@gmail.com>
2019-05-19 20:59:38 -0700

    zero index

--- src/constants/config.js
+++ src/constants/config.js
@@ -4,22 +4,22 @@ const sequenceList = [
         title: 'Sequence 1',
         noteCount: 16,
         trackList: {
-            1: {
+            0: {
                 title: 'Kick',
                 soundFile: 'kick',
                 onNotes: [0, 2, 4, 6, 8, 10, 12],
             },
-            2: {
+            1: {
                 title: 'Snare',
                 soundFile: 'snare',
                 onNotes: [2, 4, 10, 12, 15],
             },
-            3: {
+            2: {
                 title: 'Open Hat',
                 soundFile: 'hh_open',
                 onNotes: [8],
             },
-            4: {
+            3: {
                 title: 'Closed Hat',
                 soundFile: 'hh_closed',
                 onNotes: [0, 4, 5, 6, 7, 12, 14],
@@ -31,22 +31,22 @@ const sequenceList = [
         title: 'Sequence 2',
         noteCount: 16,
         trackList: {
-            1: {
+            0: {
                 title: 'Kick',
                 soundFile: 'kick',
                 onNotes: [0, 2, 4, 6, 8, 10, 12, 14],
             },
-            2: {
+            1: {
                 title: 'Snare',
                 soundFile: 'snare',
                 onNotes: [0, 4, 9],
             },
-            3: {
+            2: {
                 title: 'Open Hat',
                 soundFile: 'hh_open',
                 onNotes: [12],
             },
-            4: {
+            3: {
                 title: 'Closed Hat',
                 soundFile: 'hh_closed',
                 onNotes: [3, 7, 11],
@@ -58,25 +58,52 @@ const sequenceList = [
         title: 'Sequence 3',
         noteCount: 16,
         trackList: {
-            1: {
+            0: {
                 title: 'Kick',
                 soundFile: 'kick',
                 onNotes: [0, 4, 8, 12],
             },
-            2: {
+            1: {
                 title: 'Snare',
                 soundFile: 'snare',
                 onNotes: [4, 12],
             },
-            3: {
+            2: {
                 title: 'Open Hat',
                 soundFile: 'hh_open',
                 onNotes: [2, 6, 10, 14],
             },
-            4: {
+            3: {
                 title: 'Closed Hat',
                 soundFile: 'hh_closed',
                 onNotes: [0, 2, 4, 6, 8, 10, 12, 14],
+            }
+        }
+    },
+    {
+        id: 3,
+        title: 'Sequence 4',
+        noteCount: 16,
+        trackList: {
+            0: {
+                title: 'Kick',
+                soundFile: 'kick',
+                onNotes: [0, 4, 8, 12],
+            },
+            1: {
+                title: 'Snare',
+                soundFile: 'snare',
+                onNotes: [],
+            },
+            2: {
+                title: 'Open Hat',
+                soundFile: 'hh_open',
+                onNotes: [],
+            },
+            3: {
+                title: 'Closed Hat',
+                soundFile: 'hh_closed',
+                onNotes: [],
             }
         }
     }
