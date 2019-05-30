9a23b52aed4587a7793782c9ad4cdbf924497862
Johnny5 <johnny5@generalui.com>
2019-05-30 10:23:52 -0700

    Bring back the App =)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
-import * as serviceWorker from './utils/serviceWorker'
+import App from '../src/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-// For offline support change unregister() to register()  https://bit.ly/CRA-PWA
-serviceWorker.register()
